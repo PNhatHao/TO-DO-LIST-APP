9ede576be9b1721f6cb7c7f2bee1c35930698dd4
197CT22160 - Phạm Nhật Hào - K25T-IT5 <hao.197ct22160@vanlanguni.vn>
2023-12-02 23:40:46 +0700

    first commit

--- App.js
+++ App.js
@@ -3,64 +3,31 @@ import * as React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen} from './screens/HomeScreen'
-import { DetailsScreen} from './screens/DetailsScreen.js'
+
 import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import Icon from 'react-native-vector-icons/Ionicons'
 
-const HomeStack = createNativeStackNavigator();
-const DetailsStack = createNativeStackNavigator();
+import MainTabScreen from './screens/MainTabScreen.js';
+
+
+import { HomeScreen} from './screens/HomeScreen'
+import { DetailsScreen} from './screens/DetailsScreen.js'
+
+// const HomeStack = createNativeStackNavigator();
+// const DetailsStack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-const HomeStackScreen = ({navigation}) => (
-      <HomeStack.Navigator screenOptions={{
-        headerStyle:{
-          backgroundColor:'#009387',
-        },
-        headerTintColor:'#fff',
-        headerTitleStyle: {
-          fontWeight:'bold',
-        }
-      }}>
-        <HomeStack.Screen name="Home" component={HomeScreen} options={{
-          title:'Overview',
-          headerLeft: () => (
-            <Icon.Button name="ios-menu" size={25}
-            backgroundColor="#009387" onPress ={() => navigation.openDrawer()}
-            ></Icon.Button>
-          )
-        }} />
-      </HomeStack.Navigator>
-        
-);
 
-const DetailsStackScreen = ({navigation}) => (
-  <DetailsStack.Navigator screenOptions={{
-    headerStyle:{
-      backgroundColor:'#009387',
-    },
-    headerTintColor:'#fff',
-    headerTitleStyle: {
-      fontWeight:'bold',
-    }
-  }}>
-    <Stack.Screen name="Details" component={DetailsScreen} options={{
-          headerLeft: () => (
-            <Icon.Button name="ios-menu" size={25}
-            backgroundColor="#009387" onPress ={() => navigation.openDrawer()}
-            ></Icon.Button>
-          )
-     }} />
-  </DetailsStack.Navigator> 
-);
 
 function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeStackScreen} />
-        <Drawer.Screen name="Details" component={DetailsStackScreen} />
+        <Drawer.Screen name="Home" component={MainTabScreen} />
+        {/* <Drawer.Screen name="Details" component={DetailsStackScreen} /> */}
       </Drawer.Navigator>
 
      
